6b5fe7bac2ec1ca737b04f8ea478308a6262ee43
DawnHavoc <sarnavadas2@gmail.com>
2024-07-01 23:47:48 +0530

    commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-//hyghhewijijririreioj
+->i like you<-
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
